9aa6b81ec32223b18273085b64db369f2992b2a9
J.T. Parrish <jtparrish@outlook.com>
2024-04-22 00:11:59 -0400

    update build structure

--- flake.nix
+++ flake.nix
@@ -4,19 +4,57 @@
   };
   outputs = { self, nixpkgs }:
   let
+    ghcVersion = "ghc92";
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    overlay = final: prev: {
-      tricount = final.callCabal2nix "tricount" ./. { };
+    overlay = final: prev:
+    let
+      # previous lib
+      lib = prev.lib;
+      # previous haskell lib
+      hlib = prev.haskell.lib;
+      # apply an override to the ghc selected
+      overrideGHC = ghc: o:
+        ghc.override (old: {
+          overrides =
+            # compose the new extension
+            lib.composeExtensions
+              # if old.overrides is undefined, we take the identity
+              (old.overrides or (_: _: {}))
+              o;
+        });
+    in
+    {
+      # add the necessary packages to haskell
+      haskell = prev.haskell // {
+        # add the necessary packages
+        packages = prev.haskell.packages // {
+          # perform the GHC override
+          ${ghcVersion} = overrideGHC prev.haskell.packages.${ghcVersion} (hfinal: hprev: {
+            ### # add the alga package
+            ### alga = hlib.markUnbroken
+            ###   (hlib.overrideCabal hprev.alga {
+            ###     version = "0.7";
+            ###     sha256 = "";
+            ###     doCheck = false;
+            ###   });
+            # especially if not using flakes, should lib.cleanSourceWith with a
+            # custom filter to remove things that don't affect compilation
+            tricount = hprev.callCabal2nix "tricount" ./. { };
+          });
+        };
+      };
     };
-    myHaskellPackages = pkgs.haskellPackages.extend overlay;
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ overlay ];
+    };
   in
   {
-    devShells.${system}.default = myHaskellPackages.shellFor {
+    devShells.${system}.default = pkgs.haskell.packages.${ghcVersion}.shellFor {
       packages = p: [
         p.tricount
       ];
-      nativeBuildInputs = with myHaskellPackages; [
+      nativeBuildInputs = with pkgs.haskell.packages.${ghcVersion}; [
         ghcid
         cabal-install
         haskell-language-server
