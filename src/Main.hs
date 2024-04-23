60f1591f85a1d88a360d76949286f970eee56073
J.T. Parrish <jtparrish@outlook.com>
2024-04-22 23:23:47 -0400

    first draft of first algo

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ module Main (main) where
 -- import Data.Aeson (Result (..))
 import Data.Maybe (fromMaybe)
 import Data.Semigroup ((<>))
-import qualified Data.Text as DT
+-- import qualified Data.Text as DT
 -- import Options.Applicative hiding (Success)
 import System.Environment (lookupEnv)
 -- import Text.PrettyPrint.Boxes hiding ((<>))
@@ -11,13 +11,35 @@ import Text.Read (readMaybe)
 -- import TodoApp.Request (Task)
 -- import qualified TodoApp.Request as TR
 
-import Data.Graph as Graph
+import Algebra.Graph as LibGraph
+import Algebra.Graph (Graph)
 
+import Data.Vector as LibVector
+import Data.Vector (Vector)
+import Data.Set as LibSet
+import Data.Set (Set)
+
+-- ** allowed by PatternSynonyms
+pattern GEmpty = LibGraph.Empty
+-- apparently point free not allowed
+pattern GVertex x = LibGraph.Vertex x
+pattern GOverlay g1 g2 = LibGraph.Overlay g1 g2
+pattern GConnect g1 g2 = LibGraph.Connect g1 g2
+-- **
 
 main :: IO ()
 main = print "TODO"
 
-tcount :: Graph.Graph -> Int
-tcount g = 3
+-- int graph so we can use them as indices
+tcount :: Graph Int -> Int
+tcount g = tcountH (LibGraph.vertexCount g) (LibGraph.edgeList g)
+  where
+  -- set up the data for recursion (vector of sets)
+  tcountH n = tcountR (LibVector.replicate n LibSet.empty)
+  -- roll the update and looping
+  tcountR a ((u, v):es) = LibSet.size (LibSet.intersection (a ! u) (a ! v)) + tcountR aUpdate es
+    where
+    -- A(v) <- A(v) U {u}
+    aUpdate = a // [(v, LibSet.insert u (a ! v))]
 
 
